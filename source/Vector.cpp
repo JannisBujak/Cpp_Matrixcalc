b20fe587f0f4c1743fa72ce12a3cf4afd5c5874c
jannisbujak <jannis.bujak@gmail.com>
2018-07-22 21:56:58 +0200

    Added vector ( empty ).

---
+++ source/Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JannisB98 on 22.07.2018.
+//
+
+#include "Vector.h"
---
+++ source/Vector.h
@@ -0,0 +1,20 @@
+//
+// Created by JannisB98 on 22.07.2018.
+//
+
+#ifndef MATRIXCALC_VECTOR_H
+#define MATRIXCALC_VECTOR_H
+
+#include <vector>
+
+class Vector {
+    
+    private:
+    std::vector<int> row;
+
+};
+
+
+
+
+#endif //MATRIXCALC_VECTOR_H
